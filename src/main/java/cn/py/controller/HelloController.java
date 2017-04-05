cc86ef54a8e54f4f35fe7e5c17ca3a2c9e20346e
lihuiby <liby3@126.com>
2017-04-05 10:01:28 +0800

    log日志

--- src/main/java/cn/py/controller/HelloController.java
+++ src/main/java/cn/py/controller/HelloController.java
@@ -1,7 +1,6 @@
 package cn.py.controller;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
